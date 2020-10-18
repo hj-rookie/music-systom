3e630652258025b70a605b90d9745b96abf7efae
hj-rookie <1573786811@qq.com>
2020-10-18 14:52:38 +0800

    music1.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# music
+
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+###首页截图
+![music](https://github.com/hj-rookie/test_repository/blob/master/pic/1.PNG)
+
---
+++ src/components/02.index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="index-container">
+    <!-- 导航区域 -->
+    <div class="nav" >
+      <ul>
+        <li>
+          <router-link to="/discovery">
+            <span class="iconfont el-icon-view"></span>
+            发现音乐
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/playlists">
+            <span class="iconfont el-icon-s-operation"></span>
+            推荐歌单
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/songs">
+            <span class="iconfont el-icon-headset"></span>
+            最新音乐
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/mvs">
+            <span class="iconfont el-icon-monitor"></span>
+            最新MV
+          </router-link>
+        </li>
+      </ul>
+    </div>
+    <!-- 主体区域 -->
+    <div class="main">
+      <router-view></router-view>
+    </div>
+    <!-- 播放标签 -->
+    <div class="player">
+      <!-- autoplay 自动播放 -->
+      <audio :src='musicUrl' controls autoplay></audio>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data() {
+    return {
+      musicUrl:""
+    };
+  }
+};
+</script>
+
+<style >
+
+</style>
